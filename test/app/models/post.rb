da83b1dd53d6d9aef39c8985e93e3aa0f140aa58
Colin Young <me@colinyoung.com>
2012-10-25 23:58:27 -0500

    Trying to get travis tests to pass.

--- test/app/models/post.rb
+++ test/app/models/post.rb
@@ -1,6 +1,11 @@
 class Post
   include ActiveModel::Serialization
   include ActiveModel::MassAssignmentSecurity
+  include ActiveModel::AttributeMethods
   
   attr_accessible :body, :content
+  
+  def initialize(attributes = {})
+    @attributes = attributes
+  end
 end
