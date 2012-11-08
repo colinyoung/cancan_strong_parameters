d5ce6590de02c7909595eb1ba5fd3d217ec2c09b
Colin Young <me@colinyoung.com>
2012-11-08 16:23:18 -0600

    Fix #3 - can't start rails without gem 'cancan' and gem 'strong_parameters'

--- lib/cancan_strong_parameters.rb
+++ lib/cancan_strong_parameters.rb
@@ -1,3 +1,4 @@
+require "active_support/hash_with_indifferent_access"
 require "cancan_strong_parameters/version"
 require "cancan_strong_parameters/controller"
 require "cancan_strong_parameters/deep_permit"
